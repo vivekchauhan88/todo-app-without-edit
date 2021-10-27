7bc6dd64ad6ee58d227e411e478a748a09056c95
Vivek Chauhan <vivekchauhan67@yahoo.com>
2021-10-27 23:16:32 +0530

    Added delete and tried edit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,27 @@ import { useState, useEffect } from 'react';
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState(['Feed cow', 'Walk cow', 'Drink water'])
+  const [modify, setModify] = useState(false)
+  /* for (let i = 0; i < tasks.length; i++) {
+    modify[i] = false
+  } */
+  //console.log('modify', modify)
   const createTask = e => {
     e.preventDefault()
     console.log(task)
-    if(task) {
+    if (task) {
       //setTasks([...tasks, task])
       tasks.push(task)
       console.log(tasks)
       setTask('')
     }
+  }
+  const edit = (i, e) => {
+    setTasks([...tasks.slice(0, i), e.target.value, ...tasks.slice(i + 1, tasks.length)])
+  }
+  const remove = (i) => {
+    setTasks(tasks.slice(0, i).concat(tasks.slice(i + 1, tasks.length)))
+    console.log('deleted')
   }
 
   return (
@@ -45,17 +57,19 @@ function App() {
       </Form>
       <br /><hr />
       {
-        tasks.map(task => (
+        tasks.map((task, i) => (
           <>
-            <Row>
+            <Row key={i}>
               <Col xs={0} md={2}>
               </Col>
               <Col xs={8} md={6}>
-                <h5>{task}</h5>
+                <h5>
+                  {modify ? <input type='text' value={tasks[i]} onChange={e => edit(i, e)} /> : <p onDoubleClick={e => { setModify(true); console.log(e.target.value) }} onBlur={() => { setModify(false); console.log(tasks) }}>{task}</p>}
+                </h5>
               </Col>
               <Col xs={4} md={4} className='icons'>
-                <AiOutlineEdit className='icon' onClick={() => console.log('edited')} />{' '}
-                <AiOutlineDelete className='icon' onClick={() => console.log('deleted')} />
+                {/* <AiOutlineEdit className='icon' onClick={() => edit(i)} />{' '} */}
+                <AiOutlineDelete className='icon' onClick={() => remove(i)} />
               </Col>
             </Row>
             <hr />
