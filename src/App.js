32200e7f5050acc0f3831b8ee61d627aa144f18c
Vivek Chauhan <vivekchauhan67@yahoo.com>
2024-10-01 14:22:05 +0530

    Removed uncooked edit

--- src/App.js
+++ src/App.js
@@ -1,83 +1,75 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
-import { useState, useEffect } from 'react';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { useState } from 'react';
 
 function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState(['Feed cow', 'Walk cow', 'Drink water'])
-  const [modify, setModify] = useState(false)
-  /* for (let i = 0; i < tasks.length; i++) {
-    modify[i] = false
-  } */
-  //console.log('modify', modify)
-  const createTask = e => {
-    e.preventDefault()
-    console.log(task)
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState(['Feed cow', 'Walk cow', 'Drink water']);
+
+  const createTask = (e) => {
+    e.preventDefault();
+    console.log(task);
     if (task) {
-      //setTasks([...tasks, task])
-      tasks.push(task)
-      console.log(tasks)
-      setTask('')
+      setTasks([...tasks, task]);
+      console.log(tasks);
+      setTask('');
     }
-  }
-  const edit = (i, e) => {
-    setTasks([...tasks.slice(0, i), e.target.value, ...tasks.slice(i + 1, tasks.length)])
-  }
+  };
+
   const remove = (i) => {
-    setTasks(tasks.slice(0, i).concat(tasks.slice(i + 1, tasks.length)))
-    console.log('deleted')
-  }
+    setTasks(tasks.slice(0, i).concat(tasks.slice(i + 1, tasks.length)));
+    console.log('deleted');
+  };
 
   return (
-    <Container className="App">
+    <Container className='App'>
       <br />
       <Row align='center'>
-        <Col xs={0} md={2}>
-        </Col>
+        <Col xs={0} md={2}></Col>
         <Col xs={8} md={6}>
           <h1>To-do App</h1>
         </Col>
-        <Col xs={4} md={4}>
-        </Col>
+        <Col xs={4} md={4}></Col>
       </Row>
-      <br /><br />
+      <br />
+      <br />
       <Form onSubmit={createTask}>
         <Row>
-          <Col xs={0} md={2}>
-          </Col>
+          <Col xs={0} md={2}></Col>
           <Col xs={8} md={6}>
-            <Form.Control placeholder="Add a task" value={task} onChange={e => setTask(e.target.value)} />
+            <Form.Control
+              placeholder='Add a task'
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
           </Col>
           <Col xs={4} md={4}>
-            <Button variant="primary" type='submit'>Create</Button>
+            <Button variant='primary' type='submit'>
+              Create
+            </Button>
           </Col>
         </Row>
       </Form>
-      <br /><hr />
-      {
-        tasks.map((task, i) => (
-          <>
-            <Row key={i}>
-              <Col xs={0} md={2}>
-              </Col>
-              <Col xs={8} md={6}>
-                <h5>
-                  {modify ? <input type='text' value={tasks[i]} onChange={e => edit(i, e)} /> : <p onDoubleClick={e => { setModify(true); console.log(e.target.value) }} onBlur={() => { setModify(false); console.log(tasks) }}>{task}</p>}
-                </h5>
-              </Col>
-              <Col xs={4} md={4} className='icons'>
-                {/* <AiOutlineEdit className='icon' onClick={() => edit(i)} />{' '} */}
-                <AiOutlineDelete className='icon' onClick={() => remove(i)} />
-              </Col>
-            </Row>
-            <hr />
-          </>
-        ))
-      }
+      <br />
+      <hr />
+      {tasks.map((task, i) => (
+        <>
+          <Row key={i}>
+            <Col xs={0} md={2}></Col>
+            <Col xs={8} md={6}>
+              <h5>{task}</h5>
+            </Col>
+            <Col xs={4} md={4} className='icons'>
+              <AiOutlineDelete className='icon' onClick={() => remove(i)} />
+            </Col>
+          </Row>
+          <hr />
+        </>
+      ))}
     </Container>
-  )
+  );
 }
 
 export default App;
